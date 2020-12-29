a32793ac3c3d91931dd020a0e3baf7ac7ed70efb
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-29 22:00:06 +0300

    Create MovieAppApp.swift

---
+++ MovieApp/MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Mariam2 on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
